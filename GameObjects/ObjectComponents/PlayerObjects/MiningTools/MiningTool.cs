b69feca36a21008b77eab3a24d524c52350feb03
RawGames <RawGamesBusiness@gmail.com>
2018-07-09 15:21:09 +0200

    is this loos
    loss*

---
+++ GameObjects/ObjectComponents/PlayerObjects/MiningTools/MiningTool.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+using GameProject.GameUtils;
+using GameProject.GameObjects.ObjectComponents;
+
+namespace GameProject.GameObjects
+{
+    public class MiningTool
+    {
+        // Targeted ground object
+        Ground target;
+
+        // Collider that will check for target
+        Collider collider;
+
+        // Player object
+        PlayerObject player;
+
+        // Mining timer
+        Timer miningTimer;
+
+        // MiningStats
+        public int MiningDamage;
+        public int MiningSpeed;
+
+        // Initialize tool
+        public virtual void Initialize(PlayerObject player)
+        {
+            this.player = player;
+
+            collider = new BoxCollider();
+            if (collider is BoxCollider bc)
+            {
+                bc.Size = new Vector2(32, 32);
+                bc.Offset = new Vector2(-16, -16);
+            }
+
+            // Mining Timer
+            miningTimer = new Timer();
+
+            // stats
+            MiningDamage = 1;
+            MiningSpeed = 5;
+        }
+
+        // Determinee a target
+        public virtual void DetermineTarget()
+        {
+            Vector2 horizontalHitPoint = player.Position;
+            Vector2 verticalHitPoint = player.Position;
+
+            if (GameInput.InputDown(GameInput.Up) || GameInput.LeftStick.Y >= .2f) verticalHitPoint += new Vector2(0, -21);
+            if (GameInput.InputDown(GameInput.Down) || GameInput.LeftStick.Y <= -.2f) verticalHitPoint += new Vector2(0, 21);
+
+            if (GameInput.InputDown(GameInput.Left) || GameInput.LeftStick.X <= -.2f) horizontalHitPoint += new Vector2(-5, 0);
+            if (GameInput.InputDown(GameInput.Right) || GameInput.LeftStick.X >= .2f) horizontalHitPoint += new Vector2(5, 0);
+
+            if (CheckToolCollisin(horizontalHitPoint) is Ground hGround)
+            {
+                target = hGround;
+            }
+            else if (CheckToolCollisin(verticalHitPoint) is Ground vGround)
+            {
+                target = vGround;
+            }
+            else target = null;
+        }
+
+        // Dig 
+        public virtual void Dig()
+        {
+            miningTimer.Tick();
+            if (GameInput.InputDown(GameInput.Dig))
+            {
+                if (target != null)
+                {
+                    if (miningTimer.Check())
+                    {
+                        target.TakeDamage(MiningDamage);
+                        miningTimer.Set(MiningSpeed);
+                    }
+                }
+            }
+        }
+
+        // Draw highlight on ground
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            if (target != null)
+            {
+                ShapeRenderer.FillRectangle(spriteBatch, target.Position, new Vector2(32, 32), 0, Color.Red);
+            }
+        }
+
+        // is tool touchin anything?
+        protected virtual Ground CheckToolCollisin(Vector2 position)
+        {
+            for (int i = 0; i < player.Screen.GameObjects.Count; i++)
+            {
+                if (player.Screen.GameObjects[i] is Ground ground)
+                {
+                    if (ground.GetComponent<HitBox>().HitBoxCollider.IsColliding(collider, ground.Position, position))
+                        return ground;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ GameObjects/ObjectComponents/PlayerObjects/PlayerObject.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+using GameProject.GameUtils;
+using GameProject.GameScreens;
+using GameProject.GameObjects.ObjectComponents;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameProject.GameObjects
+{
+    public class PlayerObject : GameObject, IMovable
+    {
+        // Movement variables
+        float maxSpeed;
+        float accelerationSpeed;
+        float airAccelerationSpeed;
+        float slowDownSpeed;
+        float airSlowDownSpeed;
+
+        // Jumping
+        float maxJumpHeight;
+        float minJumpHeight;
+        int jumpBuffer; // Lets you jump even if you press button too early
+
+        // Components
+        Physics physics;
+        HitBox hitBox;
+        Sprite sprite;
+
+        // Mining tool
+        MiningTool miningTool;
+
+        public override void Initialize(GameScreen screen)
+        {
+            base.Initialize(screen);
+
+            // Components
+            physics = new Physics();
+            AddComponent(physics);
+            physics.Solid = true;
+            physics.GravityEnabled = true;
+
+            hitBox = new HitBox();
+            BoxCollider boxCollider = new BoxCollider();
+            boxCollider.Size = new Vector2(32, 64);
+            boxCollider.Offset = new Vector2(-16, -32);
+            hitBox.SetCollider(boxCollider);
+            AddComponent(hitBox);
+
+            sprite = new Sprite();
+            AddComponent(sprite);
+            sprite.AddTexture(CreateRectangle(new Vector2(32, 64), Color.Blue));
+            sprite.SpriteOffset = new Vector2(-16, -32);
+
+            // MAke this the tafget of the Screen camera
+            screen.Camera.SetTarget(this);
+
+            // Mining tool;
+            miningTool = new MiningTool();
+            miningTool.Initialize(this);
+
+            // Movement initialization
+            maxSpeed = 2;
+            accelerationSpeed = .5f;
+            airAccelerationSpeed = .25f;
+            slowDownSpeed = .25f;
+            airSlowDownSpeed = .1f;
+
+            maxJumpHeight = 6;
+            minJumpHeight = 2.5f;
+            jumpBuffer = 0;
+        }
+
+        // Update components and do other logic
+        public override void Update()
+        {
+            // Horizontal movement controlls
+            bool left = GameInput.InputDown(GameInput.Left);
+            bool right = GameInput.InputDown(GameInput.Right);
+            if (left || right)
+            {
+                HorizontalMovement(((right ? 1 : 0) - (left ? 1 : 0)) * maxSpeed);
+            }
+            else if (GameInput.LeftStick.X != 0)
+            {
+                HorizontalMovement(GameInput.LeftStick.X * maxSpeed);
+            }
+            else StopMoving();
+
+            // Jumping controlls
+            if (GameInput.InputPressed(GameInput.Jump))
+                jumpBuffer = 3;
+
+            if (physics.Grounded)
+            {
+                if (jumpBuffer > 0)
+                    Jump();
+            }
+
+            if (!GameInput.InputDown(GameInput.Jump))
+                physics.Velocity.Y = Math.Max(physics.Velocity.Y, -minJumpHeight);
+
+            jumpBuffer--;
+
+            // Mining
+            miningTool.DetermineTarget();
+            miningTool.Dig();
+
+            base.Update();
+        }
+
+        // Drawing sprite and other things
+        public override void Draw(SpriteBatch spriteBatch)
+        { 
+            base.Draw(spriteBatch);
+            miningTool.Draw(spriteBatch);
+        }
+
+        #region Movement
+
+        // Move horizontally
+        public void HorizontalMovement(float targetSpeed)
+        {
+            physics.Velocity.X = GameMath.Approach(physics.Velocity.X, targetSpeed, physics.Grounded ? accelerationSpeed : airAccelerationSpeed);
+        }
+
+        //Stop this lmao
+        public void StopMoving()
+        {
+            physics.Velocity.X = GameMath.Approach(physics.Velocity.X, 0, physics.Grounded ? slowDownSpeed : airSlowDownSpeed);
+        }
+
+        // jump
+        public void Jump()
+        {
+            physics.Velocity.Y = -maxJumpHeight;
+        }
+
+        #endregion
+    }
+}
